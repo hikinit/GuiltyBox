0e248924d9b1b789fb7a2511528bed663be630ec
mirufu <hi@rshier.com>
2019-05-18 11:04:46 +0700

    Initial Commit

---
+++ GuiltyBox/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GuiltyBox
+//
+//  Created by rshier on 18/05/19.
+//  Copyright © 2019 rshier. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
